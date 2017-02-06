d8c23b903e841e645ab79b6f01743df1480eaf85
nolan aguirre <nolanaguirre08@gmail.com>
2017-02-05 21:59:39 -0600

    add path back to router put and post

--- routes/create_account.js
+++ routes/create_account.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var pgp = require('pg-promise')( /*options*/ )
-pgp.pg.defaults.ssl = true;
-var db = pgp(process.env.DATABASE_URL);
+//pgp.pg.defaults.ssl = true;
+//var db = pgp(process.env.DATABASE_URL);
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   //console.log('Time: ', Date.now());
   next();
 });
 // define the about route
-router.put(function(req, res) {
+router.put('/',function(req, res) {
     console.log(req.body);
     db.one('SELECT EXISTS (SELECT 1 FROM users WHERE username = $1);', req.body.user.username)
         .then(function(data) {
@@ -23,7 +23,7 @@ router.put(function(req, res) {
             console.log('ERROR:' + error)
         })
 })
-router.post(function(req ,res){
+router.post('/', function(req ,res){
     console.log(req.body);
     db.none('INSERT INTO users (id, username, password) VALUES ($1, $2, $3)', [2, req.body.user.username, req.body.user.password])
         .then(function(data) {

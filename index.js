0c372a3505f14900cb95127d74c3a06dd532c009
nolan aguirre <nolanaguirre08@gmail.com>
2017-02-05 19:59:23 -0600

    remove database to test other aspects of the website

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 var pgp = require('pg-promise')( /*options*/ )
-pgp.pg.defaults.ssl = true;
-var db = pgp(process.env.DATABASE_URL);
+//pgp.pg.defaults.ssl = true;
+//var db = pgp(process.env.DATABASE_URL);
 var routes = {
     index: require('./routes/index'),
     create_account : require('./routes/create_account')
 }
-console.log(process.env.DATABASE_URL);
+//console.log(process.env.DATABASE_URL);
 app.use(bodyParser.json()); // for parsing application/json
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
